de87f98517da07cd0e69b6fc58187e68ed48cea5
10sr <8slashes+git@gmail.com>
2015-06-20 18:39:24 +0900

    Fix my-last impl

--- l99.scm
+++ l99.scm
@@ -1,10 +1,11 @@
 (define (my-last l)
   ;; TODO: Avoid error when lst is null
   ;; should use null?
-  (if (eq? (cdr l)
-          '())
-      (list (car l))
-      (my-last (cdr l))))
+  (if (null? l)
+      (error "Empty list given")
+      (if (null? (cdr l))
+          (list (car l))
+          (my-last (cdr l)))))
 
 (define (my-but-last l)
   (if (null? l)
