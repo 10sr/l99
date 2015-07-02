7865b63640ce6a201a3f1e97fe377756ddbbf4ff
10sr <8slashes+git@gmail.com>
2015-07-03 01:07:13 +0900

    Use define-syntax to define tests

--- test/run.scm
+++ test/run.scm
@@ -5,22 +5,32 @@
 ;; http://practical-scheme.net/gauche/man/gauche-refj_105.html
 ;; I want to use `test-module', but I currently do not know how to make modules
 
+;; TODO: Use `...' to accept multiple functions
+(define-syntax test-l99
+  (syntax-rules ()
+    ((_ desc from to func)
+     (test* desc
+            to
+            (apply func from)))))
+
 (test-start "l99")
 
 (test-section "Working with lists")
 
-(test* "Find the last box of a list"
-       '(d)
-       (my-last '(a b c d)))
+(test-l99 "L1: Find the last box of a list"
+          '((a b c d))
+          '(d)
+          my-last)
 
-(test* "Find the last but one box of a list"
-       '(c d)
-       (my-but-last '(a b c d)))
+(test-l99 "L2: Find the last but one box of a list"
+          '((a b c d))
+          '(c d)
+          my-but-last)
 
-(test* "Find the K'th element of a list"
-       'c
-       (element-at '(a b c d e)
-                   3))
+(test-l99 "L3: Find the K'th element of a list"
+          '((a b c d e) 3)
+          'c
+          element-at)
 
 
 ;; Somehow in travis environment cannot use :exit-on-failure
